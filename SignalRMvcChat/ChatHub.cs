da1de13531916cb33e2943b69be51c36466c41c6
Prakash <98660007+prakash-kathiresan@users.noreply.github.com>
2024-02-21 20:37:15 +0530

    update

---
+++ SignalRMvcChat/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SendToUser(string user, string receiverConnectionId, string message)
+        {
+            await Clients.Client(receiverConnectionId).SendAsync("ReceiveMessage", user, message);
+        }
+
+        public string GetConnectionId() => Context.ConnectionId;
+    }
+}
